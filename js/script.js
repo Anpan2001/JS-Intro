4ad8a3b2b12a2cc946f5fc9d689ae95ab22e7cd0
Jordy Rochín <retussu@gmail.com>
2023-05-22 16:19:46 -0600

    Ejercicio realizado el 22 de mayo

---
+++ js/script.js
@@ -0,0 +1,61 @@
+alert("hola");
+
+recipiente = "agua";
+recipiente = "café";
+recipiente = "jugo";
+
+console.log(recipiente);
+
+/* 
+salimos;
+edad;
+altura;
+peso;
+género;
+
+・String - valores en forma de texto, usar ya sean comillas simples, dobles o backticks
+・Number - valores numéricos, no usa comillas
+・Boolean - valores tipo true o false
+・Undefined - datos sin valor; variables sin inicializar
+・Null - definir que un valor es nulo de forma intencional
+・Symbol 
+・NaN (Not@Number) - valores que no son números
+・Object (no primitivo)
+*/
+
+apodoDeFelipe = "Octavo 'Hokage'";
+console.log(apodoDeFelipe);
+
+edadDeFelipe = 31;
+costoDeUnGansito = 18.50; 
+cuentaDeFelipe = 2340.56 
+
+console.log(edadDeFelipe);
+console.log(costoDeUnGansito);
+console.log(cuentaDeFelipe);
+
+esAdmin = true;
+contraseñaCorrecta = true;
+cuentaPremium = false;
+
+console.log(esAdmin);
+console.log(contraseñaCorrecta);
+console.log(costoDeUnGansito);
+
+/*
+respuestaDeCrush; //Undefined
+precioBoletoParking; //Undefined
+console.log(precioBoletoParking);
+*/
+
+respuestaDeCrush = "no";
+respuestaDeCrush = null;
+respuestaDeCrush = "si";
+console.log(respuestaDeCrush);
+//-------
+operación = 0 / 0;
+console.log(operación)
+
+//TypeOf
+console.log(typeof(esAdmin));
+console.log(typeof(edadDeFelipe));
